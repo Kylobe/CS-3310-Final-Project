76997d8099392028f465b43ca1cc85f4f6e54608
Adam Sellers <Turtle106@users.noreply.github.com>
2025-12-05 22:33:12 -0700

    Add type hints and improve MIS calculations
    Refactor main function to include type hints and additional functionality.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from typing import Dict, Set
+
 from exact_mis import exact_mis
 from greedy_mis import greedy_mis
+from local_improve import random_improve
 
 
-def main():
-    my_dict = {
+def main() -> None:
+    my_dict: Dict[str, Set[str]] = {
                 "1": set(["2", "4"]),
                 "2": set(["1", "5", "3"]),
                 "3": set(["2", "6"]),
@@ -15,12 +18,12 @@ def main():
                 "9": set(["8", "6"])
             }
     mis = exact_mis(my_dict)
-    aprox_mis = greedy_mis(my_dict)
-    print(mis)
-    print(aprox_mis)
-
-
-
+    approx_mis = greedy_mis(my_dict)
+    random_improve_mis = random_improve(my_dict)
+    print(f"Exact MIS cardinality: {len(mis)}\n\t{mis}\n")
+    print(f"Approximation MIS cardinality: {len(approx_mis)}\n\t{approx_mis}\n")
+    print(f"Random Improve MIS cardinality: {len(random_improve_mis)}\n\t{random_improve_mis}\n")
+    
 if __name__ == "__main__":
     main()
 
